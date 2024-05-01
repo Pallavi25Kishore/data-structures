63a59ed79d380c38407c2f06fc7f109987019161
Pallavi <pallavi25kishore@gmail.com>
2024-05-01 16:02:57 -0700

    "Implement functional-shared instantiation of stack"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instanceStack = {};
+  instanceStack.storage = {};
+  for (var key in stackMethods) {
+    instanceStack[key] = stackMethods[key];
+  }
+  return instanceStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    var size = 0;
+    for (var key in this.storage) {
+      size++;
+    }
+    this.storage[size + 1] = value;
+  },
+  pop: function() {
+    var size = 0;
+    for (var key in this.storage) {
+      size++;
+    }
+    var itemToRemove = this.storage[size];
+    delete this.storage[size];
+    return itemToRemove;
+  },
+  size: function() {
+    var size = 0;
+    for (var key in this.storage) {
+      size++;
+    }
+    return size;
+  }
+};
 
 
