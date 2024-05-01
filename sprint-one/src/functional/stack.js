e239bf40d7c38fb8e8fbc8ddc8820c0ab2f64a7e
Pallavi <pallavi25kishore@gmail.com>
2024-05-01 14:30:26 -0700

    "Implement functional instantiation of stack"

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var currentSize = 0;
+    for (var key in storage) {
+      currentSize++;
+    }
+    storage[currentSize + 1] = value;
   };
 
   someInstance.pop = function() {
+    var currentSize = 0;
+    for (var key in storage) {
+      currentSize++;
+    }
+    var valueToBeRemoved = storage[currentSize];
+    delete storage[currentSize];
+    return valueToBeRemoved;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      size++;
+    }
+    return size;
   };
 
   return someInstance;
