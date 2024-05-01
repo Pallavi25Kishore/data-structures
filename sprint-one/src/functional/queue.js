2c403e3ccb32b0e6e32f15b8363cc45caf792b33
Pallavi <pallavi25kishore@gmail.com>
2024-05-01 14:57:37 -0700

    "Implement functional instantiation of queue"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,41 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var currentSize = 0;
+    for (var key in storage) {
+      currentSize++;
+    }
+    var LargestKey = currentSize;
+    for (var key in storage) {
+      if (parseInt(key) > LargestKey) {
+        LargestKey = parseInt(key);
+      }
+    }
+    storage[LargestKey + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    var currentSize = 0;
+    for (var key in storage) {
+      currentSize++;
+    }
+    var smallestKey = currentSize;
+    for (var key in storage) {
+      if (parseInt(key) < smallestKey) {
+        smallestKey = parseInt(key);
+      }
+    }
+    var DequeueValue = storage[smallestKey];
+    delete storage[smallestKey];
+    return DequeueValue;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (var key in storage) {
+      size++;
+    }
+    return size;
   };
-
   return someInstance;
 };
