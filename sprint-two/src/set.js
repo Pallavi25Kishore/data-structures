ae5dd82b53333b0f50c527a28612d512cf9a8d4c
Pallavi <pallavi25kishore@gmail.com>
2024-05-03 11:28:13 -0700

    "Implement set and pass given tests"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add - O(n) - linear time complexity
+contains - O(n) - linear time complexity
+remove - O(n) - linear time complexity
  */
