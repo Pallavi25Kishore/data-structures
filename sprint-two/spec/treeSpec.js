71a79f37e8455489c56b8f521e413dcfe8d88af1
Pallavi <pallavi25kishore@gmail.com>
2024-05-04 11:16:41 -0700

    Add additional unit test to treeSpec.js

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,13 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  //additionally added unit test
+  it('should correctly detect leaf nodes with no children', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    expect(tree.children[0].children[0].children.length).to.equal(0);
+  });
+
 });
