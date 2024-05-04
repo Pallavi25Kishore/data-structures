e4acbe1f4e1bac6563fabd6985ce3f62d2e50539
Pallavi <pallavi25kishore@gmail.com>
2024-05-04 12:07:20 -0700

    Add additional function to pass additionally added unit test in setSpec.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -25,9 +25,21 @@ setPrototype.remove = function(item) {
   }
 };
 
+// adding additional function to pass unit test added additionally
+setPrototype.countValues = function(value) {
+  var counter = 0;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === value) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
 add - O(n) - linear time complexity
 contains - O(n) - linear time complexity
 remove - O(n) - linear time complexity
+countValues - additionally added function to pass added unit test - O(n);
  */
